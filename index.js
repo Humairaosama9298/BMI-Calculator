88dd2b44ee0ec63450c5caca1e120ef86e73d7b3
Humairaosama9298 <faryalfilza@gmail.com>
2024-04-24 23:47:34 +0500

    my first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+async function calculateBmi() {
+    const { weight, height } = await inquirer.prompt([
+        {
+            name: "weight",
+            type: "number",
+            message: "Enter your weight in KG"
+        },
+        { name: "height",
+            type: "number",
+            message: "Enter your height in meters"
+        }
+    ]);
+    const bmi = weight / (height * height);
+    const category = getBmiCategory(bmi);
+    console.log(`Your BMI is ${bmi} (${category})`);
+}
+function getBmiCategory(bmi) {
+    if (bmi < 18.5) {
+        return "Underweight";
+    }
+    else if (bmi < 25) {
+        return "Normal weight";
+    }
+    else if (bmi < 30) {
+        return "Overweight";
+    }
+    else {
+        return "Oobsss";
+    }
+}
+calculateBmi();
